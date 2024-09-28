949de2e5a5f5d9ada1351870decc0e51d8e1e2a4
Nur-Amin2 <rabbiii049@gmail.com>
2024-09-28 23:49:19 +0600

    fetching error fixed

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 4000;
 
 
-console.log( `User Name:${process.env.db_user}, $Password:{process.env.db_pass}` )
+console.log(`User Name:${process.env.db_user}, Password:${process.env.db_pass}`)
 
 //middleware
 
@@ -31,12 +31,53 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('carsDoc').collection('services');
+
+    app.get('/services', async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // app.get('/services/:id', async(req, res)=>{
+    //   console.log(req.params)
+    //   const id = parseInt(req.params.id);
+    //   const query = {_id: new ObjectId(id)}
+
+    //   const options = {
+    //     // Include only the `title` and `imdb` fields in the returned document
+    //     projection: {  title: 1, price: 1, service_id: 1 },
+    //   };
+
+    //   const result = await serviceCollection.findOne(query, options);
+    //   res.send(result);
+    //  })
+
+    app.get('/services/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await serviceCollection.findOne(query);
+
+        if (result) {
+          res.status(200).json(result); // Sending the lecture as JSON response
+        } else {
+          res.status(404).send("Service not found");
+        }
+      } catch (error) {
+        console.error("Error fetching Services:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -45,11 +86,11 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send("doctor is running")
+app.get('/', (req, res) => {
+  res.send("doctor is running")
 })
 
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 })
